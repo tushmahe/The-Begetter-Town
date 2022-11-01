d3e61981a85cc3315ac087887ed857fccd0c470e
Saniya Maheshwari <saniyachainchani@gmail.com>
2022-11-02 02:08:28 +0530

    SignUp page added

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.render("index");
-})
+});
+
+app.get("/signup", function(req, res){
+    res.render("signup.ejs");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
