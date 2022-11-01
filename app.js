68732a90f61a32b61d00e568a24f7a1e7f4d6565
Tusharika Maheshwari <75881715+tushmahe@users.noreply.github.com>
2022-11-02 03:09:40 +0530

    contact page

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/login", function(req, res){
 app.get("/signup", function(req, res){
     res.render("signup.ejs");
 });
+app.get("/contactUs", function(req, res){
+    res.render("contactUs.ejs");
+});
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
