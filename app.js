82bf4afbfce33f7c3680f62828845c1af1bcacb8
Saniya Maheshwari <saniyachainchani@gmail.com>
2022-11-14 03:55:05 +0530

    Picture uploading functionality added

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const methodOverride = require("method-override");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/auth');
+const { requireAuth, checkUser, checkAdmin } = require('./middleware/auth');
 const nodemailer = require('nodemailer');
+const formidable = require('formidable');
+const fileUpload = require("express-fileupload");
+
 
 const transporter = nodemailer.createTransport({
-    service:'hotmail',
+    service: 'hotmail',
     auth: {
         user: 'tushmaheshwari@outlook.com',
         pass: 'Tushmahe@123'
@@ -31,6 +34,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(fileUpload());
 
 const conn = mongoose.connect("mongodb://localhost:27017/theBegetterTownDB", {
     useNewUrlParser: true,
@@ -41,19 +45,7 @@ const conn = mongoose.connect("mongodb://localhost:27017/theBegetterTownDB", {
 
 const Profile = require("./models/profile.model");
 const Post = require("./models/post.model");
-
-const ProfileStorage = multer.diskStorage({
-    dest: function (req, file, cb) {
-        cb(null, 'profilePictures/')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now + file.originalname);
-    }
-});
-
-const profilepic = multer({
-    storage: ProfileStorage
-});
+const Event = require("./models/event.model");
 
 
 app.get("*", checkUser);
@@ -97,7 +89,7 @@ app.get("/add_post", requireAuth, function (req, res) {
 });
 
 
-app.post("/signup", profilepic.single("profilepicture"), async (req, res) => {
+app.post("/signup", async (req, res) => {
     var TypeOfUser;
     if (req.body.creator == 1) {
         TypeOfUser = "Creator";
@@ -107,6 +99,29 @@ app.post("/signup", profilepic.single("profilepicture"), async (req, res) => {
     }
 
     const password = await bcrypt.hash(req.body.password, 10);
+
+    var filename;
+
+    if (req.files && Object.keys(req.files).length !== 0) {
+
+        // Uploaded path
+        uploadedFile = req.files.uploadFile;
+        filename = Date.now() + uploadedFile.name;
+
+        // Logging uploading file
+        // console.log(uploadedFile);
+
+        // Upload path
+        const uploadPath = __dirname
+            + "/public/img/profilePic/" + filename;
+
+        // To save the file using mv() function
+        uploadedFile.mv(uploadPath);
+    }
+    else {
+        res.send("No file uploaded !!");
+    }
+
 
 
     try {
@@ -123,10 +138,7 @@ app.post("/signup", profilepic.single("profilepicture"), async (req, res) => {
             BusinessEmail: req.body.businessemail,
             Address: req.body.city + ", " + req.body.state + ", Zip Code: " + req.body.zip,
             Bio: req.body.bio,
-            ProfileImg: {
-                data: req.file,
-                contentType: "image/png"
-            }
+            ProfileImg: filename
         });
 
         const token = jwt.sign(
@@ -148,11 +160,11 @@ app.post("/signup", profilepic.single("profilepicture"), async (req, res) => {
             // }
             // const email = await Profile.findOne({Email: req.body.email});
             // if(email){
-                
+
             // }
             // const phoneno = await Profile.findOne({PhoneNumber: req.body.phonenumber});
             // if(phoneno){
-                
+
             // }
             res.send("Please make sure your username, e-mail ID and phone number are unique");
             // res.redirect("signup");
@@ -162,18 +174,18 @@ app.post("/signup", profilepic.single("profilepicture"), async (req, res) => {
 
 app.post("/contactUs", async (req, res) => {
 
-    try{
+    try {
 
-    const msg = await ContactUs.create({
-        Name: req.body.firstname + " " + req.body.lastname,
-        Email: req.body.email,
-        Message: req.body.message
-    });
+        const msg = await ContactUs.create({
+            Name: req.body.firstname + " " + req.body.lastname,
+            Email: req.body.email,
+            Message: req.body.message
+        });
 
-    // user.save().then(() => res.send("Successfully Uploaded"));
-}catch(error){
-    res.status(400).send("Error occured");
-}
+        // user.save().then(() => res.send("Successfully Uploaded"));
+    } catch (error) {
+        res.status(400).send("Error occured");
+    }
 
     res.redirect("/")
 });
@@ -198,11 +210,6 @@ app.post("/login", async (req, res) => {
             );
 
             res.cookie('jwt', token, { maxAge: 100000000000 });
-            // const all = await Post.find({});
-            // res.render("index", allPosts = all);
-            // res.render("index.ejs");
-            // return res.redirect("/").redirect("/index")
-            // res.render("index.ejs");
 
             console.log("Logged in successfully");
 
@@ -220,7 +227,7 @@ app.post("/login", async (req, res) => {
 });
 
 
-app.get("/mypost", requireAuth, async function(req, res) {
+app.get("/mypost", requireAuth, async function (req, res) {
     const token = req.cookies.jwt;
 
     jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
@@ -228,18 +235,18 @@ app.get("/mypost", requireAuth, async function(req, res) {
             console.log(err);
             // res.redirect("/login");
         }
-        else{
+        else {
             let user = await Profile.findById(decodedToken.id);
-            const all = await Post.find({Username:user.Username});
+            const all = await Post.find({ Username: user.Username });
 
             // console.log(all);
-        
-            res.render("mypost",allPosts = all);
+
+            res.render("mypost", allPosts = all);
         }
     })
-  
-   
-   
+
+
+
 });
 
 app.get("/logout", (req, res) => {
@@ -248,6 +255,9 @@ app.get("/logout", (req, res) => {
 });
 
 app.post("/add_post", (req, res) => {
+
+    console.log(req.body.title);
+
     const token = req.cookies.jwt;
 
     jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
@@ -257,18 +267,39 @@ app.post("/add_post", (req, res) => {
         }
         else {
             let user = await Profile.findById(decodedToken.id);
-            console.log("***********************************")
-             console.log(user)
-             console.log("***********************************")
+            var filename;
+
+            if (req.files && Object.keys(req.files).length !== 0) {
+
+                // Uploaded path
+                uploadedFile = req.files.uploadFile;
+                filename = Date.now() + uploadedFile.name;
+
+                // Logging uploading file
+                // console.log(uploadedFile);
+
+                // Upload path
+                const uploadPath = __dirname
+                    + "/public/img/postPic/" + filename;
+
+                // To save the file using mv() function
+                uploadedFile.mv(uploadPath);
+            }
+            else {
+                res.send("No file uploaded !!");
+            }
+
+            console.log(filename);
             const newPost = await Post.create({
                 Username: user.Username,
                 Title: req.body.title,
                 Description: req.body.description,
-                Category: user.FieldOfInterest
+                Category: user.FieldOfInterest,
+                postPicture: filename
             });
 
         }
-    })
+    });
 
     res.redirect("/");
 });
@@ -276,13 +307,27 @@ app.post("/add_post", (req, res) => {
 app.post("/deletePost", async (req, res) => {
     // console.log(req.body.title);
 
-    const thispost = await Post.findOne({Title: req.body.title});
+    const thispost = await Post.findOne({ Title: req.body.title });
 
     console.log(thispost);
 
-    Post.findOneAndRemove({Title: req.body.title}, () => {
+    Post.findOneAndRemove({ Title: req.body.title }, () => {
         res.redirect("/mypost");
     });
+});
+
+app.get("/add_event", checkAdmin, async (req, res) => {
+    res.render("add_event");
+});
+
+app.post("/add_event", async (req, res) => {
+    const newEvent = await Event.create({
+        Title: req.body.title,
+        Category: req.body.category,
+        Description: req.body.description
+    });
+
+    res.redirect("/");
 });
 
 app.listen(8080, function () {
