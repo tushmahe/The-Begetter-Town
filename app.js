70c84769dac1f170dd46b658be46c6376f4b3c25
Tusharika Maheshwari <75881715+tushmahe@users.noreply.github.com>
2022-11-15 00:12:09 +0530

    some changes

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ app.post("/login", async (req, res) => {
 
             console.log("Logged in successfully");
 
-            res.redirect("/");
+            // res.redirect("http://localhost:3000");
         }
         else {
             res.send("Incorrect Password");
